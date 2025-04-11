04eb5acce9f2e39119c11fa63e19ff9178f271c4
zainali28 <zainali123113@gmail.com>
2025-04-12 01:04:28 +0500

    fixed top margin on android

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,8 +3,8 @@ import { Colors } from "@/constants/Colors";
 import { supabase } from "@/lib/supabase";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { router } from "expo-router";
-import { Button, Image, TouchableOpacity } from "react-native";
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { Button, Image, Platform, TouchableOpacity } from "react-native";
+import { SafeAreaView, StyleSheet, Text, View, StatusBar } from "react-native";
 
 const index = () => {
   const clearStorage = async () => {
@@ -26,16 +26,16 @@ const index = () => {
       <ThreeButtonGradient />
       <View style={styles.exerciseCard}>
         <Image
-          source={require('@/assets/images/women-planking.png')}
+          source={require("@/assets/images/women-planking.png")}
           style={styles.exerciseImage}
         />
         <View style={styles.exerciseInfo}>
           <Text style={styles.exerciseLevel}>12x</Text>
-          <View style={{flexDirection: 'column', justifyContent: 'center'}}>
-          <Text style={styles.exerciseTitle}>EXERCISE OF THE DAY</Text>
-          <TouchableOpacity style={styles.exerciseBtn}>
-            <Text style={styles.exerciseBtnText}>Thrust & Plank It</Text>
-          </TouchableOpacity>
+          <View style={{ flexDirection: "column", justifyContent: "center" }}>
+            <Text style={styles.exerciseTitle}>EXERCISE OF THE DAY</Text>
+            <TouchableOpacity style={styles.exerciseBtn}>
+              <Text style={styles.exerciseBtnText}>Thrust & Plank It</Text>
+            </TouchableOpacity>
           </View>
         </View>
       </View>
@@ -52,18 +52,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: Colors.light.background,
+    marginTop: Platform.OS === "android" ? StatusBar.currentHeight : null,
   },
   exerciseCard: {
-    display: 'flex',
-    flexDirection: 'row',
+    display: "flex",
+    flexDirection: "row",
     // justifyContent: 'center',
-    backgroundColor: '#b0dffb',
+    backgroundColor: "#b0dffb",
     borderRadius: 16,
     padding: 12,
     marginBottom: 20,
-    position: 'relative',
-    width: '90%',
-    alignSelf: 'center',
+    position: "relative",
+    width: "90%",
+    alignSelf: "center",
   },
   exerciseImage: {
     width: 186,
@@ -74,38 +75,38 @@ const styles = StyleSheet.create({
     marginRight: 5,
   },
   exerciseInfo: {
-    width: '40%',
-    flexDirection: 'column',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    width: "40%",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    alignItems: "center",
   },
   exerciseTitle: {
     fontSize: 20,
-    fontWeight: '800',
-    fontFamily: 'bold',
+    fontWeight: "800",
+    fontFamily: "bold",
     color: Colors.light.primary_colors.dark_navy,
     marginTop: 80,
   },
   exerciseBtn: {
     backgroundColor: Colors.light.primary_colors.dark_navy,
-    borderColor: '#4FC3F7',
+    borderColor: "#4FC3F7",
     borderWidth: 1,
     borderRadius: 12,
     paddingVertical: 6,
     paddingHorizontal: 10,
   },
   exerciseBtnText: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 10,
-    fontFamily: 'regular',
-    fontWeight: '500'
+    fontFamily: "regular",
+    fontWeight: "500",
   },
   exerciseLevel: {
-    position: 'absolute',
+    position: "absolute",
     top: 8,
     right: 12,
-    backgroundColor: '#000',
-    color: '#fff',
+    backgroundColor: "#000",
+    color: "#fff",
     paddingHorizontal: 6,
     paddingVertical: 2,
     borderRadius: 12,
